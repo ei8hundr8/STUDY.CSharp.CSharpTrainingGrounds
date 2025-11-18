db5d216ddb530505d77a4a1750c712f587be08f4
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-19 00:31:11 +0800

    feat: implement basic gold addition logic

--- RoyalCalculator/Program.cs
+++ RoyalCalculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Royal Calculator
+Console.WriteLine("--- Gold Addition Tool ---");
+
+Console.WriteLine("Enter the first amount of gold:");
+string input1 =  Console.ReadLine();
+
+Console.WriteLine("Enter the second amount of gold:");
+string input2 = Console.ReadLine();
+
+// Convert the strings into numbers.
+int gold1 = int.Parse(input1);
+int gold2 = int.Parse(input2);
+
+int total = gold1 + gold2;
+
+Console.WriteLine($"Total gold: {total}");
+Console.WriteLine("Don't spend it all in one tavern!");
