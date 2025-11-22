8193f0c19d4759593d463799845a09c610424cf0
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-22 16:09:53 +0800

    study: complete basic string formatting

---
+++ FoundationalCsWithMsCertification/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FoundationalCsWithMsCertification
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string projectName = "ACME";
+			string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+			/*Expected Output:
+			 * View English output:
+					c:\Exercise\ACME\data.txt
+
+				Посмотреть русский вывод:
+					c:\Exercise\ACME\ru-RU\data.txt
+			 */
+
+			string locEng = @$"c:\Exercise\{projectName}\data.txt";
+			string locRus = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+
+			Console.WriteLine($"View English output:\n\t{locEng}\n");
+			Console.WriteLine($"View {russianMessage} output:\n\t{locRus}\n");
+
+		}
+	}
+}
