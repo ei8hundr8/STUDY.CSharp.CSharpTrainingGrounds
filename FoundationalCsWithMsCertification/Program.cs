3cb7d67aa120bc5295a0a3dbadff561332a74370
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-23 16:15:39 +0800

    study: else if subscription checker

--- FoundationalCsWithMsCertification/Program.cs
+++ FoundationalCsWithMsCertification/Program.cs
@@ -4,49 +4,53 @@
 	{
 		static void Main(string[] args)
 		{
-			Random dice = new Random();
+			/*
+			 * Review the business rules for this challenge
+			 * Rule 1: Your code should only display one message.
+			 *	The message that your code displays will depend on the other five rules. For rules 2-6, the higher numbered rules take precedence over the lower numbered rules.
+			 * Rule 2: If the user's subscription will expire in 10 days or less, display the message:
+			 *	Your subscription will expire soon. Renew now!
+			 * Rule 3: If the user's subscription will expire in five days or less, display the messages:
+			 *	Your subscription expires in _ days.
+			 *	Renew now and save 10%!
+			 * Rule 4: If the user's subscription will expire in one day, display the messages:
+			 *  Your subscription expires within a day!
+			 *  Renew now and save 20%!
+			 * Rule 5: If the user's subscription has expired, display the message:
+			 *  Your subscription has expired.
+			 * Rule 6: If the user's subscription doesn't expire in 10 days or less, display nothing.
+			*/
+			Random random = new Random();
+			int daysUntilExpiration = random.Next(12);
+			int discountPercentage = 0;
 
-			int roll1 = dice.Next(1, 7);
-			int roll2 = dice.Next(1, 7);
-			int roll3 = dice.Next(1, 7);
+			// Your code goes here
 
-			int total = roll1 + roll2 + roll3;
-
-			Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-			if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+			if (daysUntilExpiration == 0)
 			{
-				if ((roll1 == roll2) && (roll2 == roll3))
-				{
-					Console.WriteLine("You rolled triples!  +6 bonus to total!");
-					total += 6;
-				}
-				else
-				{
-					Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-					total += 2;
-				}
-
-				Console.WriteLine($"Your total including the bonus: {total}");
+				Console.WriteLine("Your subscription has expired.");
+			}
+			else if (daysUntilExpiration == 1)
+			{
+				Console.WriteLine($"Your subscriptions expires within a day!");
+				discountPercentage = 20;
+			}
+			else if (daysUntilExpiration <= 5)
+			{
+				Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+				discountPercentage = 10;
+			}
+			else if (daysUntilExpiration <= 10)
+			{
+				Console.WriteLine("Your subscription will expire soon. Renew now!");
+			}
+			
+			if (discountPercentage > 0)
+			{
+				Console.WriteLine($"Renew now and save {discountPercentage}%!");
 			}
 
-			if (total >= 16)
-			{
-				Console.WriteLine("You win a new car!");
-			}
-			else if (total >= 10)
-			{
-				Console.WriteLine("You win a new laptop!");
-			}
-			else if (total == 7)
-			{
-				Console.WriteLine("You win a trip for two!");
-			}
-			else
-			{
-				Console.WriteLine("You win a kitten!");
-			}
 		}
-		
+
 	}
 }
