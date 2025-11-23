3e6b6c8af7d82949912574ef206699b2e84bc1ca
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-24 00:40:34 +0800

    study: create console app - grading exams

--- FoundationalCsWithMsCertification/Program.cs
+++ FoundationalCsWithMsCertification/Program.cs
@@ -4,35 +4,116 @@
 	{
 		static void Main(string[] args)
 		{
-			/*
-			 * Code readability challenge
-			 * In this challenge, you'll use the techniques you learned in this module to improve the readability of a code sample. 
-			 * You are provided with a code sample that is poorly styled and commented. 
-			 * Your goal is to update the code using style guidelines for variable names, code comments, and whitespace to improve code readability.
-			*/
+			// initialize variables - graded assignments 
+			int examAssignments = 5;
 
-			/*
-			 * The code below reverse the sentence, counts the number of times a certain character appears, and finally output both to the terminal.
-			 */
-			string str = "The quick brown fox jumps over the lazy dog.";
-			
-			char[] charMessage = str.ToCharArray();
-			Array.Reverse(charMessage);
-			
-			int x = 0;
+			int[] sophiaScores = [90, 86, 87, 98, 100, 94, 90];
+			int[] andrewScores = [92, 89, 81, 96, 90, 89];
+			int[] emmaScores = [90, 85, 87, 98, 68, 89, 89, 89];
+			int[] loganScores = [90, 95, 87, 88, 96, 96];
+			int[] beckyScores = [92, 91, 90, 91, 92, 92, 92];
+			int[] chrisScores = [84, 86, 88, 90, 92, 94, 96, 98];
+			int[] ericScores = [80, 90, 100, 80, 90, 100, 80, 90];
+			int[] gregorScores = [91, 91, 91, 91, 91, 91, 91];
 
-			foreach (char i in charMessage) 
-				{ 
-					if (i == 'o') 
-						{ 
-							x++;
-						} 
+			string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor"];
+			int[] studentScores = new int[10];
+
+			string currentStudentLetterGrade = "";
+
+			Console.WriteLine("Student\t\tGrade\n");
+
+			foreach (string name in studentNames)
+			{
+				string currentStudent = name;
+
+				if (currentStudent == "Sophia")
+					studentScores = sophiaScores;
+
+				else if (currentStudent == "Andrew")
+					studentScores = andrewScores;
+
+				else if (currentStudent == "Emma")
+					studentScores = emmaScores;
+
+				else if (currentStudent == "Logan")
+					studentScores = loganScores;
+
+				else if (currentStudent == "Becky")
+					studentScores = beckyScores;
+
+				else if (currentStudent == "Chris")
+					studentScores = chrisScores;
+
+				else if (currentStudent == "Eric")
+					studentScores = ericScores;
+
+				else if (currentStudent == "Gregor")
+					studentScores = gregorScores;
+
+				int sumAssignmentScores = 0;
+
+				decimal currentStudentGrade;
+
+				int gradedAssignments = 0;
+
+				foreach (int score in studentScores)
+				{
+					gradedAssignments++;
+					// when the grade ass. number is less or equal to the exam ass., that means it's the exam score, not the extra grades.
+					if (gradedAssignments <= examAssignments)
+						sumAssignmentScores += score;
+					else
+						// add the extra credit points to the sum - bonus points equal to 10% of an exam score
+						sumAssignmentScores += score / 10;
 				}
 
-			string new_message = new String(charMessage);
+				currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
-			Console.WriteLine(new_message);
-			Console.WriteLine($"'o' appears {x} times.");
+				if (currentStudentGrade >= 97)
+					currentStudentLetterGrade = "A+";
+
+				else if (currentStudentGrade >= 93)
+					currentStudentLetterGrade = "A";
+
+				else if (currentStudentGrade >= 90)
+					currentStudentLetterGrade = "A-";
+
+				else if (currentStudentGrade >= 87)
+					currentStudentLetterGrade = "B+";
+
+				else if (currentStudentGrade >= 83)
+					currentStudentLetterGrade = "B";
+
+				else if (currentStudentGrade >= 80)
+					currentStudentLetterGrade = "B-";
+
+				else if (currentStudentGrade >= 77)
+					currentStudentLetterGrade = "C+";
+
+				else if (currentStudentGrade >= 73)
+					currentStudentLetterGrade = "C";
+
+				else if (currentStudentGrade >= 70)
+					currentStudentLetterGrade = "C-";
+
+				else if (currentStudentGrade >= 67)
+					currentStudentLetterGrade = "D+";
+
+				else if (currentStudentGrade >= 63)
+					currentStudentLetterGrade = "D";
+
+				else if (currentStudentGrade >= 60)
+					currentStudentLetterGrade = "D-";
+
+				else
+					currentStudentLetterGrade = "F";
+
+				Console.WriteLine($"{currentStudent}:\t\t" + currentStudentGrade + "\t" + currentStudentLetterGrade);
+			}
+
+			Console.WriteLine("Press the Enter key to continue");
+			Console.ReadLine();
 
 		}
 	}
