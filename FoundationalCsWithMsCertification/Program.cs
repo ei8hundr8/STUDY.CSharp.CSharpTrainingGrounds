f9b1a0f550d909abff51ed11cff7150e71f77281
Ei8 <9668999+ei8hundr8@users.noreply.github.com>
2025-11-23 16:48:11 +0800

    study: foreach inventory check

--- FoundationalCsWithMsCertification/Program.cs
+++ FoundationalCsWithMsCertification/Program.cs
@@ -5,51 +5,30 @@
 		static void Main(string[] args)
 		{
 			/*
-			 * Review the business rules for this challenge
-			 * Rule 1: Your code should only display one message.
-			 *	The message that your code displays will depend on the other five rules. For rules 2-6, the higher numbered rules take precedence over the lower numbered rules.
-			 * Rule 2: If the user's subscription will expire in 10 days or less, display the message:
-			 *	Your subscription will expire soon. Renew now!
-			 * Rule 3: If the user's subscription will expire in five days or less, display the messages:
-			 *	Your subscription expires in _ days.
-			 *	Renew now and save 10%!
-			 * Rule 4: If the user's subscription will expire in one day, display the messages:
-			 *  Your subscription expires within a day!
-			 *  Renew now and save 20%!
-			 * Rule 5: If the user's subscription has expired, display the message:
-			 *  Your subscription has expired.
-			 * Rule 6: If the user's subscription doesn't expire in 10 days or less, display nothing.
+			 * Suppose you work for a manufacturing company. The company needs you to complete an inventory of your warehouse to determine the number of products that are ready to ship. 
+			 * In addition to the total number of finished products, you need to report the number of finished products stored in each individual bin in your warehouse, along with a running total. 
+			 * This running total will be used to create an audit trail so you can double-check your work and identify "shrinkage".
+			 * 
+			 * Expected Output:
+			 * Bin 1 = 200 items (Running total: 200)
+			 * Bin 2 = 450 items (Running total: 650)
+			 * Bin 3 = 700 items (Running total: 1350)
+			 * Bin 4 = 175 items (Running total: 1525)
+			 * Bin 5 = 250 items (Running total: 1775)
+			 * We have 1775 items in inventory.
 			*/
-			Random random = new Random();
-			int daysUntilExpiration = random.Next(12);
-			int discountPercentage = 0;
+			int[] inventory = [200, 450, 700, 175, 250];
+			int sum = 0;
+			int bin = 0;
 
-			// Your code goes here
-
-			if (daysUntilExpiration == 0)
+			foreach (int item in inventory)
 			{
-				Console.WriteLine("Your subscription has expired.");
-			}
-			else if (daysUntilExpiration == 1)
-			{
-				Console.WriteLine($"Your subscriptions expires within a day!");
-				discountPercentage = 20;
-			}
-			else if (daysUntilExpiration <= 5)
-			{
-				Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-				discountPercentage = 10;
-			}
-			else if (daysUntilExpiration <= 10)
-			{
-				Console.WriteLine("Your subscription will expire soon. Renew now!");
-			}
-			
-			if (discountPercentage > 0)
-			{
-				Console.WriteLine($"Renew now and save {discountPercentage}%!");
+				sum += item;
+				bin++;
+				Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
 			}
 
+			Console.WriteLine($"We have {sum} items in inventory.");
 		}
 
 	}
